4526b83811f53863306007de25689e7917b56ced
Zijie Wang <zwang578@asu.edu>
2023-09-12 17:22:22 -0700

    edit

--- questionableQA_transformation_llm_prompt.py
+++ questionableQA_transformation_llm_prompt.py
@@ -7,6 +7,8 @@ import json
 import pandas as pd
 from llama import Llama
 from typing import List
+from torch.distributed.elastic.multiprocessing.errors import record
+import torch
 
 def data_process_boolq(path):
     yn_question_list = []
@@ -50,40 +52,31 @@ def generate_prompt(question: str, number:str) -> str:
     return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
     
-### Instruction:
-I need to you to help me transform yes-no questions into wh-questions. Wh-questions are questions start with any of the following words: "Which", "What", "Who", "When", "Where", "Why" and "How". You should keep as much information from the original questions as possible. 
-{number} examples are given for references.
-### Examples:
-
-#Yes-no question: "Can betta fish survive without oxygen?"
-#new question: "How do betta fish survive without oxygen?"
-
-#Yes-no question: "Is there a duke of oxford?"
-#new question: "Who is the duke of oxford?"
-
-#Yes-no question: "Does finn die in star wars?"
-#new question: "when does finn die in star wars?"
-
-#Yes-no question: "Was titanic built in southampton?"
-#new question: "Where was the titanic built southampton?"
+Transform yes-no questions into questions start with any of the following words: "Which", "What", "Who", "When", "Where", "Why" and "How". You should keep as much information from the original questions as possible. 
 
 
-### Input:
-Yes-no question: "{question}?" 
+#Yes-no question: "Can betta fish survive without oxygen?" => #new question: "How do betta fish survive without oxygen?"
 
-### Response:"""
+#Yes-no question: "Is there a duke of oxford?" => #new question: "Who is the duke of oxford?"
+
+#Yes-no question: "Does finn die in star wars?" => #new question: "when does finn die in star wars?"
+
+#Yes-no question: "Was titanic built in southampton?" => #new question: "Where was the titanic built southampton?"
+
+#Yes-no question: "{question.capitalize()}?" => #new question:"""
 
 
+@record
 def main(
-    ckpt_dir: str,
-    tokenizer_path: str,
+    ckpt_dir: str="llama-2-7b/",
+    tokenizer_path: str="tokenizer.model",
     dataset_path: str = "dataset/",
     store_path: str = "transformation_llama_results/",
     few_shot_number: int = 4,
     temperature: float = 0.6,
     top_p: float = 0.9,
-    max_seq_len: int = 128,
-    max_gen_len: int = 64,
+    max_seq_len: int = 512,
+    max_gen_len: int = 16,
     max_batch_size: int = 4,
 ):
     """
@@ -129,21 +122,26 @@ def main(
     #     cheese =>""",
     # ]
     task = f'alpaca_prompting_transform_ynquestion_to_whquestion_boolq_{few_shot_number}_shot'
-    prompts = []
-    for yn_question, label in zip(yn_questions,labels):
-        
-        prompt = generate_prompt(yn_question, few_shot_number)
-        prompts.append(prompt)
+    prompts_list = []
+    results = []
+    for batch in range(int(len(yn_questions)/max_batch_size)):
+        prompts = []                                                        
+
+        for yn_question, label in zip(yn_questions[batch*max_batch_size:(batch+1)*max_batch_size],labels[batch*max_batch_size:(batch+1)*max_batch_size]):
+            prompt = generate_prompt(yn_question, few_shot_number)
+            prompts.append(prompt)
+            prompts_list.append(prompt)                                              
+
        
-       
-    results = generator.text_completion(
-        prompts,
-        max_gen_len=max_gen_len,
-        temperature=temperature,
-        top_p=top_p,
-    )
-    
-    for prompt, result,yn_question, label in zip(prompts, results,yn_questions,labels):
+        result = generator.text_completion(
+            prompts,
+            max_gen_len=max_gen_len,
+            temperature=temperature,
+            top_p=top_p,
+        )
+        results.extend(result)
+        #print(f"number of prompts: {len(prompts)}")
+    for prompt, result,yn_question, label in zip(prompts_list, results,yn_questions,labels):
         print(f"----------PROMPT-----------\n{prompt}\n----------END-------------\n")
         print(label)
         print(f"> {result['generation']}")
@@ -157,4 +155,6 @@ def main(
             wf.close()
 
 if __name__ == "__main__":
+    #torch.set_num_threads=1
     fire.Fire(main)
+
